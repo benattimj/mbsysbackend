50a4de61e93cd92e20b18e22d6604c2a3c00387c
Murilo Benatti <benatti1104@gmail.com>
2022-09-17 02:31:49 -0300

    Projeto criado

---
+++ src/main/java/com/mbsystems/mysts/MystsApplication.java
@@ -0,0 +1,13 @@
+package com.mbsystems.mysts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbsystems/mysts/MystsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbsystems.mysts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
