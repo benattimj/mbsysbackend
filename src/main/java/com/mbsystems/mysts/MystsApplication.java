222a8014698b81341d4a515c5cded34fd7f8e445
Murilo Benatti <benatti1104@gmail.com>
2022-09-18 22:28:45 -0300

    Cliente, Tipocliente, Telefones e enderecos

--- src/main/java/com/mbsystems/mysts/MystsApplication.java
+++ src/main/java/com/mbsystems/mysts/MystsApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mbsystems.mysts.domain.Categoria;
 import com.mbsystems.mysts.domain.Cidade;
+import com.mbsystems.mysts.domain.Cliente;
+import com.mbsystems.mysts.domain.Endereco;
 import com.mbsystems.mysts.domain.Estado;
 import com.mbsystems.mysts.domain.Produto;
+import com.mbsystems.mysts.domain.enums.TipoCliente;
 import com.mbsystems.mysts.repositories.CategoriaRepository;
 import com.mbsystems.mysts.repositories.CidadeRepository;
+import com.mbsystems.mysts.repositories.ClienteRepository;
+import com.mbsystems.mysts.repositories.EnderecoRepository;
 import com.mbsystems.mysts.repositories.EstadoRepository;
 import com.mbsystems.mysts.repositories.ProdutoRepository;
 
@@ -28,7 +33,11 @@ public class MystsApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MystsApplication.class, args);
 	}
@@ -66,6 +75,21 @@ public class MystsApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","198569845625",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("268896565","15648492"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores", "300","Apto 303","Jardim","262626262", cli1,c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro","3898959",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		
+		
+		
+	
 	}
 
 }
---
+++ src/main/java/com/mbsystems/mysts/repositories/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.mbsystems.mysts.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mbsystems.mysts.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+	
+}
