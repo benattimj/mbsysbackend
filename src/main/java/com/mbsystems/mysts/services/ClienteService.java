11a05196191debd9746d09046e140af42449ff4c
Murilo Benatti <benatti1104@gmail.com>
2022-09-20 20:46:57 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/mbsystems/mysts/services/ClienteService.java
+++ src/main/java/com/mbsystems/mysts/services/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.mbsystems.mysts.domain.Cidade;
 import com.mbsystems.mysts.domain.Cliente;
+import com.mbsystems.mysts.domain.Endereco;
+import com.mbsystems.mysts.domain.enums.TipoCliente;
 import com.mbsystems.mysts.dto.ClienteDTO;
+import com.mbsystems.mysts.dto.ClienteNewDTO;
+import com.mbsystems.mysts.repositories.CidadeRepository;
 import com.mbsystems.mysts.repositories.ClienteRepository;
+import com.mbsystems.mysts.repositories.EnderecoRepository;
 
 @Service
 public class ClienteService {
@@ -20,47 +26,71 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
-	public Cliente find(Integer id) { 
-		 Optional<Cliente> obj = repo.findById(id); 
-			 return obj.orElse(null); 
-	}
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
-	public Cliente update (Cliente obj) {
-		Cliente newObj = find(obj.getId());
-		updateData(newObj,obj);
-		return repo.save(newObj);
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElse(null);
 	}
 
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-	}
-		catch (DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityViolationException("Não é possivel excluir porque há entidades relacionadas!");
 		}
 	}
-	
-	public List<Cliente> findAll(){
+
+	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-			
-		
+
 	}
-	
+
 	public Cliente fromDTO(ClienteDTO objDto) {
-		return new Cliente (objDto.getId(), objDto.getNome(), objDto.getEmail(),null,null);
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
-	
+
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogadouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+	}
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
+
 }
