173f9555a4ec2b4837250d1784b179631a95ffbe
Murilo Benatti <benatti1104@gmail.com>
2022-09-22 13:26:51 -0300

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/mbsystems/mysts/dto/ClienteDTO.java
+++ src/main/java/com/mbsystems/mysts/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.mbsystems.mysts.domain.Cliente;
+import com.mbsystems.mysts.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
