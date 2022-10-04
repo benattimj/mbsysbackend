05c177a516c6978d7a98c4b8d4017023fce957d8
Murilo Benatti <benatti1104@gmail.com>
2022-10-04 14:20:03 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/mbsystems/mysts/services/DBService.java
+++ src/main/java/com/mbsystems/mysts/services/DBService.java
@@ -20,6 +20,7 @@ import com.mbsystems.mysts.domain.PagamentoComCartao;
 import com.mbsystems.mysts.domain.Pedido;
 import com.mbsystems.mysts.domain.Produto;
 import com.mbsystems.mysts.domain.enums.EstadoPagamento;
+import com.mbsystems.mysts.domain.enums.Perfil;
 import com.mbsystems.mysts.domain.enums.TipoCliente;
 import com.mbsystems.mysts.repositories.CategoriaRepository;
 import com.mbsystems.mysts.repositories.CidadeRepository;
@@ -127,13 +128,21 @@ public void instantiateTestDatabase() throws ParseException {
 	Cliente cli1 = new Cliente(null,"Maria Silva","benatti1104@gmail.com","198569845625",TipoCliente.PESSOAFISICA, pe.encode("123"));
 	cli1.getTelefones().addAll(Arrays.asList("268896565","15648492"));
 	
+	Cliente cli2 = new Cliente(null,"Ana Costa","murilopiloto33@gmail.com","62182165002",TipoCliente.PESSOAFISICA, pe.encode("123"));
+	cli2.addPerfil(Perfil.ADMIN);
+	cli2.getTelefones().addAll(Arrays.asList("268896544","4448492"));
+	
+	
 	Endereco e1 = new Endereco(null,"Rua Flores", "300","Apto 303","Jardim","262626262", cli1,c1);
 	Endereco e2 = new Endereco(null, "Avenida Matos", "105", "sala 800", "Centro","3898959",cli1,c2);
+	Endereco e3 = new Endereco(null, "Floriano","2106", null, "Centro","3844959",cli1,c2);
+	
 	
 	cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+	cli2.getEnderecos().addAll(Arrays.asList(e3));
 	
-	clienteRepository.saveAll(Arrays.asList(cli1));
-	enderecoRepository.saveAll(Arrays.asList(e1,e2));
+	clienteRepository.saveAll(Arrays.asList(cli1,cli2));
+	enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 	
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy HH:mm");
 	
