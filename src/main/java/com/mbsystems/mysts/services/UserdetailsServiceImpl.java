23f7e7e72bc7a6ab008ee81f89e6ba99bd7fb9d6
Murilo Benatti <benatti1104@gmail.com>
2022-10-04 15:26:42 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/mbsystems/mysts/services/UserdetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.mbsystems.mysts.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mbsystems.mysts.domain.Cliente;
+import com.mbsystems.mysts.repositories.ClienteRepository;
+import com.mbsystems.mysts.security.UserSS;
+
+
+@Service
+public class UserdetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+		
+	}
+
+}
