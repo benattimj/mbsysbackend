348a3efb7dae123bc6aa268c1a0d48cadda9cbf5
Murilo Benatti <benatti1104@gmail.com>
2022-10-05 12:14:51 -0300

    Implementando autorizacao

--- src/main/java/com/mbsystems/mysts/config/SecurityConfig.java
+++ src/main/java/com/mbsystems/mysts/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mbsystems.mysts.security.JWTAuthenticationFilter;
+import com.mbsystems.mysts.security.JWTAuthorizationFilter;
 import com.mbsystems.mysts.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
