927015005d3b2983aaf3a06580f1b9d37bb9fbb3
Murilo Benatti <benatti1104@gmail.com>
2022-10-06 23:14:45 -0300

    Esqueci a senha

--- src/main/java/com/mbsystems/mysts/services/EmailService.java
+++ src/main/java/com/mbsystems/mysts/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.mbsystems.mysts.domain.Cliente;
 import com.mbsystems.mysts.domain.Pedido;
 
 public interface EmailService {
@@ -16,5 +17,6 @@ public interface EmailService {
 	
 	void sendHtmlEmail(MimeMessage msg);
 	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 }
