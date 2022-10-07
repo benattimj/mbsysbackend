da0266607831816dd1453de9b3921aa596148466
Murilo Benatti <benatti1104@gmail.com>
2022-10-06 21:36:54 -0300

    Restricao de conteudo: Cliente recupera seus pedidos

--- src/main/java/com/mbsystems/mysts/services/PedidoService.java
+++ src/main/java/com/mbsystems/mysts/services/PedidoService.java
@@ -4,15 +4,22 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.mbsystems.mysts.domain.Cliente;
 import com.mbsystems.mysts.domain.ItemPedido;
 import com.mbsystems.mysts.domain.PagamentoComBoleto;
 import com.mbsystems.mysts.domain.Pedido;
 import com.mbsystems.mysts.domain.enums.EstadoPagamento;
+import com.mbsystems.mysts.repositories.ClienteRepository;
 import com.mbsystems.mysts.repositories.ItemPedidoRepository;
 import com.mbsystems.mysts.repositories.PagamentoRepository;
 import com.mbsystems.mysts.repositories.PedidoRepository;
+import com.mbsystems.mysts.security.UserSS;
+import com.mbsystems.mysts.services.exceptions.AuthorizationException;
 import com.mbsystems.mysts.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -72,5 +79,15 @@ public class PedidoService {
 			return obj;
 	}
 	
-	
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+}
+		
 }
